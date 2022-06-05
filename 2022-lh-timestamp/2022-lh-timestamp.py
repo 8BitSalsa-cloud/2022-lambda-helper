4372474db0cc2f104cfa14b8aa830f616e328138
jsalsa <ebassist@web.de>
2022-06-05 17:46:44 +0200

    timestamp-function
    creates a timestamp

---
+++ 2022-lh-timestamp/2022-lh-timestamp.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+# fuinction
+def get_utc_timestamp(with_decimal: bool=False):               
+    epoch = datetime(1970,1,1,0,0,0) 
+    now = datetime.utcnow() 
+    timestamp = (now - epoch).total_seconds() 
+    if with_decimal: 
+        return timestamp 
+    return int(timestamp) 
+
+# check
+print(get_utc_timestamp())
---
+++ 2022-lh-timestamp/README.md
@@ -0,0 +1,9 @@
+# 2022-lh-timestamp 
+
+# description
+
+With this funktion you create a timestamp. 
+Counting in seconds garanty a big number when starting in the pass.
+
+## usecase
+
